9bede01feaf436a88a4e2e6b68bd2789f85a5ad0
znecu <alma_maria@ucne.edu.do>
2024-11-05 17:32:50 -0400

    Llamé al método AddRoles

--- BlazorAuthorization/Program.cs
+++ BlazorAuthorization/Program.cs
@@ -29,6 +29,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
